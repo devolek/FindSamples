2b57b065696d2bcabef064ea23394acdfb3c9846
Devolek <degustator.chudes@gmail.com>
2020-04-05 18:03:02 +0300

    Added ability to parse all files in a folder, add field "type" in Sample

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,56 @@
 import org.apache.poi.ss.usermodel.*;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main
 {
-    private static String file = "C:\\Users\\Valet\\OneDrive\\журнал 2\\РазноеК-1.xlsx";
-    public static void main(String[] args)
-    {
+    private static String file = "C:\\Users\\Valet\\OneDrive\\журнал 2";
+    public static void main(String[] args) throws IOException {
         ArrayList<Sample> samples = new ArrayList<>();
-        try
-        {
-            FileInputStream inp = new FileInputStream(file);
-            Workbook workbook = WorkbookFactory.create(inp);
-            for (Sheet sheet : workbook){
-                for (int i = sheet.getFirstRowNum(); i < sheet.getLastRowNum(); i++){
-                    Row row = sheet.getRow(i);
-                    if (row == null)
-                    {
-                        continue;
+        List<File> allFiles;
+
+        allFiles = Files.walk(Paths.get(file))
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+
+        allFiles.forEach(file1 -> {
+            try {
+                Workbook workbook = WorkbookFactory.create(file1);
+                for (Sheet sheet : workbook){
+                    for (int i = sheet.getFirstRowNum(); i < sheet.getLastRowNum(); i++){
+                        Row row = sheet.getRow(i);
+                        if (row == null)
+                        {
+                            continue;
+                        }
+                        Cell cell = row.getCell(1);
+                        if (cell == null || cell.getCellType() != CellType.STRING){
+                            continue;
+                        }
+                        if (cell.getStringCellValue().isEmpty() || cell.getStringCellValue().replaceAll(" ", "").length() < row.getCell(1).toString().length())
+                        {
+                            continue;
+                        }
+                        String[] type = file1.getPath().split("\\\\");
+                        Sample sample = new Sample(sheet.getSheetName(), cell.getStringCellValue(), type[type.length - 1]);
+                        samples.add(sample);
                     }
-                    Cell cell = row.getCell(1);
-                    if (cell == null || cell.getCellType() != CellType.STRING){
-                        continue;
-                    }
-                    if (cell == null || cell.getStringCellValue().isEmpty() || cell.getStringCellValue().replaceAll(" ", "").length()
-                            < row.getCell(1).toString().length())
-                    {
-                        continue;
-                    }
-                    Sample sample = new Sample(sheet.getSheetName(), cell.getStringCellValue());
-                    samples.add(sample);
                 }
+                workbook.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
-        catch (Exception ex){
-            ex.printStackTrace();
-        }
+        });
+
         samples.forEach(System.out::println);
     }
 }
--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,13 +1,15 @@
 public class Sample
 {
+    private String type;
     private String art;
     private String instance;
     private double pH;
 
-    public Sample(String art, String instance)
+    public Sample(String art, String instance, String type)
     {
         this.art = art;
         this.instance = instance;
+        this.type = type;
     }
 
     public String getArt() {
@@ -22,8 +24,16 @@ public class Sample
         return pH;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public String toString()
     {
-        return art + " - " + instance;
+        return type + " - " + art + " - " + instance;
     }
 }
